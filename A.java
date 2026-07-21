1581c6496f64a58b7b74c41e610fb0c7f21572c2
hardik8770 <guptahardik9770@gmail.com>
2026-07-22 00:17:24 +0530

    v1 commit

--- A.java
+++ A.java
@@ -3,18 +3,18 @@ import java.util.*;
 class A{
 	public static void main(String[] args){
 		//case: 1 
-		/*int i = 0;
+		int i = 0;
 
 		for (i = 1 ; i <= 10 ; i++ ){
 			System.out.println(i);
 		}
 		System.out.println("End for loop at i :"+ i);
-		*/
+		
 
 		//case:2
-		Scanner sc = new Scanner(System.in);
+		/*Scanner sc = new Scanner(System.in);
 		System.out.println("Enter the value of n : ");
-		int n = sc.nextInt();
+		int n = sc.nextInt();*/
 
 		/*for(int i = 0; i < n ; i++){
 			for(int j = 0; j < n ; j++){
