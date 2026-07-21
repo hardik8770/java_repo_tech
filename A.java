3d68cafc54e80aeae0bfca8dd8927159d4a535ed
hardik8770 <guptahardik9770@gmail.com>
2026-07-22 00:23:30 +0530

    v2-commit

--- A.java
+++ A.java
@@ -3,13 +3,13 @@ import java.util.*;
 class A{
 	public static void main(String[] args){
 		//case: 1 
-		/*int i = 0;
+		int i = 0;
 
 		for (i = 1 ; i <= 10 ; i++ ){
 			System.out.println(i);
 		}
 		System.out.println("End for loop at i :"+ i);
-		*/
+		
 
 		//case:2
 		Scanner sc = new Scanner(System.in);
